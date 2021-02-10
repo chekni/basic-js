1f0b27ebf15c0f68f47bdc93bde8a32f61ba5272
chekni <karina_cheshko@mail.ru>
2021-02-10 08:08:57 +0300

    fix: easely solution transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -2,7 +2,7 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
     if (arr) {
-        const cloneArr = [...arr];
+        const array = [...arr];
         let result = [];
         const rule = new Map([
             ['--discard-next', 1],
@@ -12,47 +12,43 @@ module.exports = function transform(arr) {
         ]);
 
         let flag = 0;
-        let next = 1;
-        for (let i = 0; i < cloneArr.length - 1; i++) {
-            let cur = cloneArr[i];
-            next = cloneArr[i + 1];
+        let previousValue;
 
-            let action = rule.get(next);
-            if (cur && action && flag != 1) {
-                if (flag == 2 || action == -2) {
-                    result.push(cur);
-                    result.push(cur);
-                    i++;
-                    flag = 0;
+        for (let i = 0; i < array.length; i++) {
+
+            if (!rule.get(array[i])) {
+                result.push(array[i]);
+                previousValue = array[i];
+
+            } else {
+                flag = rule.get(array[i]);
+
+                switch (flag) {
+                    case -1:
+                        if (i > 2 && rule.get(array[i - 2]) != 1) result.pop();
+                        break;
+                    case -2:
+                        if (previousValue) result.push(previousValue);
+                        break;
+                    case 1:
+                        if (i !== array.length - 1) {
+                            i++;
+                        }
+                        break;
+                    case 2:
+                        if (i !== array.length - 1) {
+                            result.push(array[i + 1]);
+                        }
+                        break;
+
+                    default:
+                        break;
                 }
-                if (flag == 1 || action == -1) {
-                    i++;
-                    flag = 0;
-                }
-                if (action == 1) {
-                    result.push(cur);
-                    flag = 1;
-                }
-                if (action == 2) {
-                    result.push(cur);
-                    flag = 2;
-                }
-            } else if (typeof cur !== 'string') {
-                result.push(cur);
             }
-
-        }
-        if (flag && typeof next !== 'string') {
-            result.push(cloneArr[cloneArr.length - 1]);
         }
         return result;
     }
     return new Error();
 };
 
-// console.log(transform([1, 2, 3, '--double-next', 1337, '--double-prev', 4, 5]));
-
-// ['--discard-prev', 1, 2, 3],
-//                 ['--double-prev', 1, 2, 3],
-//                 [1, 2, 3, '--double-next'],
-//                 [1, 2, 3, '--discard-next']
+// console.log(transform([1, 2, 3, '--double-next', 1337, '--discard-prev', 4, 5]));
